b4be1cd8a9c604d1ad83594a5be9d080b80c1670
Perry Mitchell <perry@perrymitchell.net>
2015-12-19 13:51:43 +0200

    Grunt setup and index map to window

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    "use strict";
+
+    grunt.initConfig({
+
+        exec: {
+            browserify: {
+                //cmd: 'NODE_PATH=node_modules/buttercup/source node node_modules/browserify/bin/cmd.js -e node_modules/buttercup/source/module.js --insert-globals -o build/buttercup.js'
+                cmd: 'node node_modules/browserify/bin/cmd.js -e source/index.js --ignore curlrequest -t brfs --insert-globals -o build/buttercup.js'
+            }
+        },
+
+        uglify: {
+            buttercup: {
+                files: {
+                    "build/buttercup.min.js": ["build/buttercup.js"]
+                }
+            }
+        }
+
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask("default", ["build"]);
+
+    grunt.registerTask("build", [
+        "exec:browserify",
+        "uglify:buttercup"
+    ]);
+
+};
