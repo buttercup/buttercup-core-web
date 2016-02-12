cff35bc6c8349da0de4c3ca21db3cf7eb3e5742b
Perry Mitchell <perry@perrymitchell.net>
2016-02-12 21:41:49 +0200

    Added archive manager

---
+++ source/ArchiveManager.js
@@ -0,0 +1,39 @@
+(function(module) {
+
+    "use strict";
+
+    var Buttercup = require("buttercup"),
+        Credentials = Buttercup.Credentials;
+
+    var ArchiveManager = function() {
+        this._archives = {};
+    };
+
+    ArchiveManager.prototype.addArchiveCredentials = function(name, credentials) {
+        this._archives[name] = {
+            status: (typeof credentials === "string") ?
+                ArchiveManager.ArchiveStatus.LOCKED :
+                ArchiveManager.ArchiveStatus.UNLOCKED,
+            credentials: credentials
+        };
+    };
+
+    ArchiveManager.prototype.isLocked = function(name) {
+        return this._archives[name].status === ArchiveManager.ArchiveStatus.LOCKED;
+    };
+
+    ArchiveManager.prototype.unlock = function(name, password) {
+        var archiveDetails = this._archives[name],
+            credentials = Credentials.fromSecureContent(archiveDetails.credentials, password);
+        if (!credentials) {
+            throw new Error("Failed unlocking credentials: " + name);
+        }
+        archiveDetails.credentials = credentials;
+    };
+
+    ArchiveManager.ArchiveStatus = {
+        LOCKED: "locked",
+        UNLOCKED: "unlocked"
+    };
+
+})(module);
--- source/index.js
+++ source/index.js
@@ -4,4 +4,12 @@
 
 	window.Buttercup = require("buttercup");
 
+	var ArchiveManager = require("./ArchiveManager.js");
+
+	window.Buttercup.Web = {
+
+		archiveManager: new ArchiveManager()
+
+	};
+
 })();
