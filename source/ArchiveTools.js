24d6c2643d4a0be6b1035a32c20264f7d62f107b
Perry Mitchell <perry@perrymitchell.net>
2016-03-28 17:45:30 +0300

    Entry by URL filtering

--- source/ArchiveTools.js
+++ source/ArchiveTools.js
@@ -7,7 +7,13 @@
         getEntriesForURL: function(archive, url) {
             var entries = [];
             ["URL", "url"].forEach(function(metaKey) {
-                var newEntries = archive.findEntriesByMeta(metaKey, url);
+                var newEntries = archive
+                    .findEntriesByMeta(metaKey, /.+/)
+                    .filter(function(entry) {
+                        var entryURL = entry.getMeta(metaKey);
+                        console.log("ENTRY", entry, entryURL);
+                        return (url.toLowerCase().indexOf(entryURL.toLowerCase()));
+                    });
                 if (newEntries.length > 0) {
                     entries = entries.concat(newEntries);
                 }
