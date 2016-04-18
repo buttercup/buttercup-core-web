b9c4b775dfa541840b65e14403d220f5c698a916
Perry Mitchell <perry@perrymitchell.net>
2016-04-18 20:41:09 +0300

    Improved testing

---
+++ jasdriver.config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+    //closeDriverOnFinish: false,
+    //exitOnFinish: false,
+    specs: [
+        __dirname + "/build/buttercup.js",
+        __dirname + "/tests"
+    ]
+};
