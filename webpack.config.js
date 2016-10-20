7de94cfe1b894db7e1036e4d914dad5f35340fa3
Perry Mitchell <perry@perrymitchell.net>
2016-10-20 11:40:32 +0300

    Fix webpack and publishing

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                loader: "babel-loader"
+                loader: "babel"
             }
         ]
     },
