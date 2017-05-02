2ac27baf7eb5274abe3df4d95c3b49a26091da27
Perry Mitchell <perry@perrymitchell.net>
2017-05-02 20:06:06 +0300

    Remove ArchiveManager + StorageInterface

--- source/index.js
+++ source/index.js
@@ -2,11 +2,9 @@
 
 const Buttercup = require("buttercup");
 
-const ArchiveManager = require("__buttercup_web/ArchiveManager.js");
 const ArchiveTools = require("__buttercup_web/ArchiveTools.js");
 const EntryFinder = require("__buttercup_web/EntryFinder.js");
 const HashingTools = require("__buttercup_web/HashingTools.js");
-const StorageInterface = require("__buttercup_web/StorageInterface.js");
 const DropboxDatasource = require("__buttercup_web/DropboxDatasource.js");
 
 module.exports = Object.assign(
@@ -14,12 +12,10 @@ module.exports = Object.assign(
     Buttercup,
     {
         Web: {
-            ArchiveManager,
             ArchiveTools,
             DropboxDatasource,
             EntryFinder,
-            HashingTools,
-            StorageInterface
+            HashingTools
         }
     }
 );
