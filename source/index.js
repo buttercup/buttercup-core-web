e3aa8b008a579569c8437aa236b200f01716e492
Perry Mitchell <perry@perrymitchell.net>
2017-06-07 21:19:48 +0300

    Add archive manager back

--- source/index.js
+++ source/index.js
@@ -8,16 +8,22 @@ const HashingTools = require("__buttercup_web/HashingTools.js");
 const DropboxDatasource = require("__buttercup_web/DropboxDatasource.js");
 const LocalStorageInterface = require("__buttercup_web/LocalStorageInterface.js");
 
+// Deprecated:
+const ArchiveManager = require("__buttercup_web/ArchiveManager.js");
+const StorageInterface = require("__buttercup_web/StorageInterface.js");
+
 module.exports = Object.assign(
     {},
     Buttercup,
     {
         Web: {
+            ArchiveManager,
             ArchiveTools,
             DropboxDatasource,
             EntryFinder,
             HashingTools,
-            LocalStorageInterface
+            LocalStorageInterface,
+            StorageInterface
         }
     }
 );
